be06240994983c56446c6edd5bf14f5cad4fadfc
Adrian925492 <adrian.9254@gmail.com>
2020-04-02 09:53:20 +0200

    more assetions

--- Source/SuitableAssertionsTest.cc
+++ Source/SuitableAssertionsTest.cc
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
 #include <stdexcept>
+#include <float.h>
 
 TEST(SuitableAssertions, ExplicitAssertionsTest)
 {
@@ -61,4 +64,67 @@ TEST(SuitableAssertions, PredictateAssertionTest)
 {
     EXPECT_PRED1(predFunc1, 12);
     EXPECT_PRED2(predFunc2, 12, 12);    //Predictate assertions test
+}
+
+/* User defined assertion results */
+::testing::AssertionResult isEven(int i)
+{
+    if ((i%2) == 0)
+        return ::testing::AssertionSuccess();
+    else
+        return ::testing::AssertionFailure() << i << " is Odd" ;    
+}
+
+TEST(SuitableAssertions, ReturneAssertionTest)
+{
+    EXPECT_TRUE(isEven(2));
+}
+
+/* Predictate formatter */
+bool isEven2(int i)
+{
+     return ((i%2) == 0); 
+}
+
+::testing::AssertionResult AssertIsEven(const char* m_expr, int m)
+{
+    if (isEven2(m))
+    {
+        return ::testing::AssertionSuccess();
+    }
+    return ::testing::AssertionFailure() << m_expr << " is not Even! ";
+}
+
+TEST(SuitableAssertions, PrimeFormatterAssertionTest)
+{
+    EXPECT_PRED_FORMAT1(AssertIsEven, 2);   //Will result printing out own assertion fail text! 
+}
+
+/* Floating point assertions */
+TEST(SuitableAssertions, FloatAssertionTest)
+{
+    float a = 3.14;
+    float b = 3.140000001;
+
+    EXPECT_FLOAT_EQ(a, b);  //A nd B are ALMOST equal (expect_eq would return failure)
+
+    double c = 3.14;
+    double d = 3.140000000000001;
+
+    EXPECT_DOUBLE_EQ(c, d);
+}
+
+/* Assertion using gmock matcher */
+using ::testing::StartsWith;
+
+TEST(SuitableAssertions, GmockMatcherAssertionTest)
+{
+    std::string word = "HelloWorld";
+
+    EXPECT_THAT(word, StartsWith("Hello"));     //Use "startwith" matcher, case sensitive!
+}
+/* Type assertion */
+TEST(SuitableAssertions, StaticAssertionTest)
+{
+    ::testing::StaticAssertTypeEq<int, int>();  //Call compile error if types are not fit together
 }
